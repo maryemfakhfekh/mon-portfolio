2b36387d81d83e5de91f6eba66000c825cbcf694
Maryem <maryemfakhfekh1@gmail.com>
2026-03-13 23:35:06 +0100

    Update portfolio content

--- src/components/Accueil/Hero.jsx
+++ src/components/Accueil/Hero.jsx
@@ -11,7 +11,7 @@ import {
   FaDownload,
 } from "react-icons/fa";
 import maryemPhoto from "../../assets/maryem.jpg";
-import cvFile from "../../assets/maryemCV.pdf";
+import cvFile from "../../assets/MaryemFakhfekhCV.pdf";
 
 const Hero = () => {
   const user = {
